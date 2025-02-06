cbdffbf41ec4cd2fdc915939d7f2ded3e3e111b2
Andrew Roth <a.roth.80@hotmail.com>
2025-02-05 19:42:30 -0800

    another query and an sql fix/seed the options missing descriptions

--- db/queries/01_users_and_their_polls.sql
+++ db/queries/01_users_and_their_polls.sql
@@ -1,7 +1,5 @@
 SELECT
-  users.id AS user_id,
   users.email AS user_email,
-  polls.id AS poll_id,
   polls.title AS poll_title,
   polls.sub_id AS poll_link
 FROM
---
+++ db/queries/03_option_id_titles_descriptions.sql
@@ -0,0 +1,6 @@
+SELECT
+  id AS option_id,
+  title AS option_title,
+  description AS option_description
+FROM
+  options;
