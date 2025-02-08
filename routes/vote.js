3304a4c7de1f7c4d0d99cb3c8c23ee7b6dcf0b1a
Amgad Abdullah <amga200d@gmail.com>
2025-02-08 03:50:19 -0700

    created a queiry to retrive  a poll with its options from the db and showing the sortable poll to the voter

--- routes/vote.js
+++ routes/vote.js
@@ -1,17 +1,42 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const db = require('../db/connection');
 
 router.get('/', (req, res) => {
   console.log('vote: fire!');
-  db.query('SELECT * FROM polls WHERE id = 1;')
-  .then((data) => {
-    console.log(data.rows);
-    const templateVars = {polls: data.rows};
-  res.render('voting_poll',templateVars);
-  }).catch((err) => {
-    console.error(err);
-  });
+
+  // Query to fetch the poll and its associated options
+  const query = `
+    SELECT polls.id AS poll_id, polls.title AS poll_title, options.id AS option_id, options.title AS option_title, options.description AS option_description
+    FROM polls
+    JOIN options ON polls.id = options.poll_id
+    WHERE polls.id = $1;
+  `;
+
+  db.query(query, [2])
+    .then(result => {
+      if (result.rows.length === 0) {
+        return res.send("Poll not found");
+      }
+
+      // Construct the poll object with its options
+      const poll = {
+        id: result.rows[0].poll_id,
+        title: result.rows[0].poll_title,
+        options: result.rows.map(row => ({
+          id: row.option_id,
+          title: row.option_title,
+          description: row.option_description
+        }))
+      };
+
+      const templateVars = { poll };
+      res.render('voting_poll', templateVars);
+    })
+    .catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
