b660f5b2cc5a109ec1c23e3fbff567eed08bab73
Amgad Abdullah <amga200d@gmail.com>
2025-02-10 20:41:33 -0700

    fixed vote schema

--- db/schema/04_votes.sql
+++ db/schema/04_votes.sql
@@ -4,8 +4,6 @@ CREATE TABLE votes (
   rank INT[] NOT NULL,
   user_id INT NOT NULL,
   poll_id INT NOT NULL,
-  options_id INT NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (poll_id) REFERENCES polls(id),
-  FOREIGN KEY (options_id) REFERENCES options(id)
-);
+  FOREIGN KEY (poll_id) REFERENCES polls(id)
+  )
